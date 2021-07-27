1c63297e82a4ecb365ca674d419ac9582cf02c85
Dev Doug <Doug4less@Gmail.com>
2021-07-27 16:46:38 -0700

    - Add auth interceptor - Add missing annotation

--- app/src/main/java/com/walmartsample/di/NetworkModule.kt
+++ app/src/main/java/com/walmartsample/di/NetworkModule.kt
@@ -3,11 +3,16 @@ package com.walmartsample.di
 import com.walmartsample.BuildConfig
 import com.walmartsample.Config
 import com.walmartsample.network.Authintercepter
+import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+@Module
+@InstallIn(ApplicationComponent::class)
 object NetworkModule {
 
     private const val baseUrl = Config.BASE_URL

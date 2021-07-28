09a71c1fef53a2cf983cf98d2248d795200010fa
Dev Doug <Doug4less@Gmail.com>
2021-07-27 23:01:39 -0700

    -add view model - init view model - add layout xml - start observing movie id to update selected when changed

--- app/src/main/java/com/walmartsample/ui/details/DetailsActivity.kt
+++ app/src/main/java/com/walmartsample/ui/details/DetailsActivity.kt
@@ -1,11 +1,61 @@
 package com.walmartsample.ui.details
 
+import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.google.android.material.snackbar.Snackbar
+import com.walmartsample.R
 import dagger.hilt.android.AndroidEntryPoint
+import io.buildwithnd.demotmdb.model.MovieDesc
+import com.walmartsample.model.Result
+import kotlinx.android.synthetic.main.activity_details.*
+
 
 @AndroidEntryPoint
 class DetailsActivity : AppCompatActivity() {
 
     private val viewModel by viewModels<DetailsViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+
+    private fun subscribeUi() {
+        viewModel.movie.observe(this, Observer { result ->
+
+            when (result.status) {
+                Result.Status.SUCCESS -> {
+                    result.data?.let {
+                        //updateUi(it)
+                    }
+                    loading.visibility = View.GONE
+                }
+
+                Result.Status.ERROR -> {
+                    result.message?.let {
+                        showError(it)
+                    }
+                    loading.visibility = View.GONE
+                }
+
+                Result.Status.LOADING -> {
+                    loading.visibility = View.VISIBLE
+                }
+            }
+        })
+    }
+
+    private fun showError(msg: String) {
+        Snackbar.make(vParent, msg, Snackbar.LENGTH_INDEFINITE).setAction("DISMISS") {
+        }.show()
+    }
+
+    companion object {
+        const val EXTRAS_MOVIE_ID = "movie_id"
+    }
+
 
 }
--- app/src/main/java/com/walmartsample/ui/details/DetailsViewModel.kt
+++ app/src/main/java/com/walmartsample/ui/details/DetailsViewModel.kt
@@ -1,9 +1,30 @@
 package com.walmartsample.ui.details
 
-import androidx.lifecycle.ViewModel
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.*
+import com.walmartsample.data.MovieRepository
+import io.buildwithnd.demotmdb.model.MovieDesc
+import kotlinx.coroutines.flow.onStart
+import com.walmartsample.model.Result
+import kotlinx.coroutines.flow.collect
+
 
 /**
  * ViewModel for Movie details screen
  */
-class DetailsViewModel : ViewModel() {
+class DetailsViewModel  @ViewModelInject constructor(private val movieRepository: MovieRepository) : ViewModel() {
+
+    private var _id = MutableLiveData<Int>()
+    private val _movie: LiveData<Result<MovieDesc>> = _id.distinctUntilChanged().switchMap {
+        liveData {
+            movieRepository.fetchMovie(it).onStart {
+                emit(Result.loading())
+            }.collect { emit(it) }
+        }
+    }
+    val movie = _movie
+
+    fun getMovieDetail(id: Int) {
+        _id.value = id
+    }
 }

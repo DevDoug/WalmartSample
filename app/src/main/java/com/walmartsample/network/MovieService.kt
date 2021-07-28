a91da18a4e0593fed7d778def84d8275ad52220d
Dev Doug <Doug4less@Gmail.com>
2021-07-27 21:51:56 -0700

    - api endpoints from retrofit and results from api calls

--- app/src/main/java/com/walmartsample/network/MovieService.kt
+++ app/src/main/java/com/walmartsample/network/MovieService.kt
@@ -1,4 +1,15 @@
 package com.walmartsample.network
 
+import com.walmartsample.model.TrendingMovieResponse
+import io.buildwithnd.demotmdb.model.MovieDesc
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface MovieService {
+    @GET("/3/trending/movie/week")
+    suspend fun getPopularMovies() : Response<TrendingMovieResponse>
+
+    @GET("/3/movie/{movie_id}")
+    suspend fun getMovie(@Path("movie_id") id: Int) : Response<MovieDesc>
 }

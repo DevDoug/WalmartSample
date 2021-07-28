e26b41f6f5330ea3adb7f197cf8fd845d74e8034
Dev Doug <Doug4less@Gmail.com>
2021-07-27 23:48:28 -0700

    - add view model

--- app/src/main/java/com/walmartsample/ui/listing/ListingViewModel.kt
+++ app/src/main/java/com/walmartsample/ui/listing/ListingViewModel.kt
@@ -1,9 +1,33 @@
 package com.walmartsample.ui.listing
 
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.walmartsample.data.MovieRepository
+import com.walmartsample.model.TrendingMovieResponse
+import kotlinx.coroutines.flow.collect
+import com.walmartsample.model.Result
+import kotlinx.coroutines.launch
 
 /**
  * ViewModel for ListingActivity
  */
-class ListingViewModel : ViewModel() {
+class ListingViewModel @ViewModelInject constructor(private val movieRepository: MovieRepository) :
+    ViewModel() {
+
+    private val _movieList = MutableLiveData<Result<TrendingMovieResponse>>()
+    val movieList = _movieList
+
+    init {
+        fetchMovies()
+    }
+
+    private fun fetchMovies() {
+        viewModelScope.launch {
+            movieRepository.fetchTrendingMovies().collect {
+                _movieList.value = it
+            }
+        }
+    }
 }

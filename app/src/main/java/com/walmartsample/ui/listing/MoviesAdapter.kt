c0df141b697374d96621a35cc6c02c361eca86f1
Dev Doug <Doug4less@Gmail.com>
2021-07-27 23:52:33 -0700

    - add movie adapter

--- app/src/main/java/com/walmartsample/ui/listing/MoviesAdapter.kt
+++ app/src/main/java/com/walmartsample/ui/listing/MoviesAdapter.kt
@@ -1,24 +1,53 @@
 package com.walmartsample.ui.listing
 
 import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.walmartsample.Config
+import com.walmartsample.R
+import com.walmartsample.model.Movie
+import com.walmartsample.ui.details.DetailsActivity
+import com.walmartsample.util.Genre
+import kotlinx.android.synthetic.main.list_item_movie.view.*
 
-class MoviesAdapter() : RecyclerView.Adapter<MoviesAdapter.MovieViewHolder>() {
+class MoviesAdapter(private val context: Context, private val list: ArrayList<Movie>) :
+    RecyclerView.Adapter<MoviesAdapter.MovieViewHolder>() {
 
     class MovieViewHolder(private val context: Context, itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(movie: Movie) {
+            itemView.setOnClickListener {
+                val intent = Intent(context, DetailsActivity::class.java)
+                intent.putExtra(DetailsActivity.EXTRAS_MOVIE_ID, movie.id)
+                context.startActivity(intent)
+            }
+            itemView.tvTitle.text = movie.title
+            Glide.with(context).load(Config.IMAGE_URL + movie.poster_path)
+                .apply(RequestOptions().override(400, 400).centerInside().placeholder(R.drawable.placehoder)).into(itemView.ivPoster)
+            itemView.tvGenre.text = Genre.getGenre(movie.genre_ids)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
-        TODO("Not yet implemented")
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.list_item_movie, parent, false)
+        return MovieViewHolder(context, view)
     }
+
+    override fun getItemCount(): Int = list.size
 
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(list[position])
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+    fun updateData(newList: List<Movie>) {
+        list.clear()
+        val sortedList = newList.sortedBy { movie -> movie.popularity }
+        list.addAll(sortedList)
+        notifyDataSetChanged()
     }
-}
